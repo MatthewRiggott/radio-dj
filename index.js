c14053d873033e77a8504cd977e9edbf269705d0
Matt <Matthew.S.Riggott@gmail.com>
2020-03-19 16:23:34 -0400

    reformat proj, add workspace settings

--- index.js
+++ index.js
@@ -5,41 +5,60 @@
  */
 const express = require("express");
 const path = require("path");
-
 const createHost = require("./src/Host.js");
+
 /**
  * App Variables
  */
 const app = express();
 app.use(express.static(__dirname + '/html'));
-var expressWs = require('express-ws')(app);
+
+const server = require("http").createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || "8000";
+const debug = process.env.DEBUG || true
+
 /**
  *  App Configuration
  */
 var OpenConnections = []
+
 /**
  * Routes Definitions
  */
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/index.html'))
+  res.sendFile(path.join(__dirname + '/html/index.html'))
 });
 
 app.get("/host", (req, res) => {
-    console.debug(createHost)
-    let newHost = createHost.createHost({ hostIp: "local" })
-    OpenConnections.push(newHost)
-    res.status(200).send(`Host URL ${newHost.code}\r\nConnections: ${OpenConnections.length}`);
+  let newHost = createHost.createHost({ hostIp: "local" })
+  OpenConnections.push(newHost)
+  res.status(200).send(`Host URL ${newHost.code}\r\nConnections: ${OpenConnections.length}`);
 });
 
-app.ws('/echo', function(ws, req) {
-    ws.on('message', function(msg) {
-        ws.send(msg);
-    });
-})
 /**
  * Server Activation
  */
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+
+// attach socket server
+io.attach(server, {
+  pingInterval: 10,
+  pingTimeout: 5,
+  cookie: false
 });
+
+server.listen(port, () => {
+  writeDebug(`Listening to requests on http://localhost:${port}`);
+});
+
+function wDebug(msg) {
+  if (debug) {
+    console.debug(msg);
+  }
+}
+
+function wDir(obj) {
+  if (debug) {
+    console.dir(obj, 2, true)
+  }
+}
